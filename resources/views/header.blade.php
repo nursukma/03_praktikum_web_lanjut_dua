4fea965c2397c49eb54a7d9dc4ade8b1eff96b4d
bagustri9 <bagustriwidiyanto9@gmail.com>
2021-03-02 14:56:52 +0700

    layout dasar 2

---
+++ resources/views/header.blade.php
@@ -0,0 +1,23 @@
+<div class="site-header">
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="navbar-header">
+                    <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#main-menu">
+                        <span class="sr-only">Toggle navigation</span>
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand" href="index.html">Stone</a>
+                </div>
+                <div class="collapse navbar-collapse" id="main-menu">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.html">Home</a></li>
+                        <li><a href="promotion.html">Promotion</a></li>
+                        <li><a href="products.html">Products</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>         
+        </div>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,145 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<div class="site-slider">
+    <div class="flexslider">
+        <ul class="slides">
+            <li class="slide">
+                <div class="overlay"></div>
+                <img src="{{asset('images/slide1.jpg')}}" alt="">
+                <div class="slider-caption">
+                    <div class="title">
+                        <h2>WEB DESIGN</h2>
+                        <h2>INSPIRATION</h2>
+                    </div>
+                    <a href="#" class="slider-button">Download</a>
                 </div>
+            </li>
+            <li class="slide">
+                <div class="overlay"></div>
+                <img src="{{asset('images/slide2.jpg')}}" alt="">
+                <div class="slider-caption">
+                    <div class="title">
+                        <h2>DEVELOPMENT</h2>
+                        <h2>TEMPLATEMO</h2>
+                    </div>
+                    <a href="#" class="slider-button">Read More</a>
+                </div>
+            </li>
+        </ul>
+    </div>   
+</div>
+<div class="first-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h3>Responsive Design</h3>
+                <p>Stone template is provided by <a href="http://www.templatemo.com" target="_parent">templatemo</a> website. Credits go to Smashing Magazine for <a href="http://www.smashingmagazine.com/2012/11/20/art-professions-icons-png/">Art Professions Icon Set</a> and <a href="http://unsplash.com">Unsplash</a> for images. Praesent imperdiet orci se ante vehicula pulvinar. Morbi adipiscing molestie iaculis. Maecenas accumsan gravida est, quis placerat neque ullamcorper vitae. 
+              <br><br>Aenean cursus non neque at porta. Lorem ipsum dolor  amet, consectetur adipiscing elit. Praesent massa justo, eleifend ut purus sed, pellentesque scelerisque ligula. Fusce ullamcorper rutrum est, nec ullamcorper velit cursus eget.</p>
+            </div>
+            <div class="col-md-6 text-center">
+                <div class="image-holder">
+                    <img src="{{asset('images/1.png')}}" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="second-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-6 text-center">
+                <img src="{{asset('images/s1.png')}}" alt="">
+                <h4>DRAW THE SCATCH</h4>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <img src="{{asset('images/s2.png')}}" alt="">
+                <h4>MIX IT UP</h4>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <img src="{{asset('images/s3.png')}}" alt="">
+                <h4>TAKE A PHOTO</h4>
+            </div>
+            <div class="col-md-3 col-sm-6 text-center">
+                <img src="{{asset('images/s4.png')}}" alt="">
+                <h4>PREVIEW PRINT</h4>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="third-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>First Procedure</h2>
+                <span>(Pellentesque ligula lectus)</span>
+                <p>Praesent sit amet cursus urna. In nec lobortis tellus. Donec congue, urna non semper interdum, que nisi bibendum tellus, sed tempus tellus neque et leo. Cras laoreet mi sed nulla lobortis, epharetra eros laoreet.
+                <br><br>Maecenas vitae ornare arcu. Phasellus gravida congue turpis, eu bibendum lorem molestie eget. Phasellus tristique sapien non placerat eleifend. Nam quis velit non quam bibendum vestibulum ac et purus.</p>
+            </div>
+            <div class="col-md-6">
+                <h2>Second Procedure</h2>
+                <span>(In nec lobortis tellus)</span>
+                <ol class="no-color">
+                    <li>Pellentesque ligula lectus, molestie quis magna lobortis, placerat imperdiet elit.</li>
+                    <li>Praesent sit amet cursus urna. In nec lobortis tellus.</li>
+                    <li>Donec congue, urna non semper interdum, que nisi bibendum tellus.</li>
+                    <li>Cras laoreet mi sed nulla lobortis, epharetra eros laoreet.</li>
+                    <li>Donec convallis elementum convallis. Duis ac purus sem. Fusce id nunc tellus.</li>
+                </ol>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Third Procedure</h2>
+                <span>(Donec convallis elementum convallis)</span>
+                <ol>
+                    <li>
+                        <h4>Pellentesque ligula lectus, molestie quis magna lobortis.</h4>
+                        <p>Praesent sit amet cursus urna. In nec lobortis tellus. Donec congue, urna non semper         interdum, que nisi bibendum tellus, sed tempus tellus neque et leo. </p>
+                    </li>
+                    <li>
+                        <h4>Cras laoreet mi sed nulla lobortis, epharetra eros laoreet.</h4>
+                        <p>Donec convallis elementum convallis. Duis ac purus sem. Fusce id nunc tellus. Maecenas vitae ornare arcu. Phasellus gravida congue turpis, eu bibendum.</p>
+                    </li>
+                    <li>
+                        <h4>Lorem molestie eget tristique sapien non placerat eleifend. </h4>
+                        <p>Nam quis velit non quam bibendum vestibulum ac et purus. Fusce id nunc tellus. Maecenas vitae ornare arcu.</p>
+                    </li>
+                </ol>
+            </div>
+            <div class="col-md-6">
+                <h2>Fourth Procedure</h2>
+                <span>(Nam quis velit non quam bibendum)</span>
+                <ol>
+                    <li>
+                        <h4>Pellentesque ligula lectus, molestie quis magna lobortis.</h4>
+                        <ol class="no-color">
+                            <li>Praesent sit amet cursus urna. In nec lobortis tellus.</li>
+                            <li>Donec congue, urna non semper interdum.</li>
+                            <li>Que nisi bibendum tellus, sed tempus tellus neque et leo.</li>
+                            <li>Cras laoreet mi sed nulla lobortis, epharetra eros laoreet.</li>
+                        </ol>
+                    </li>
+                    <li>
+                        <h4>Donec convallis elementum convallis. Duis ac purus sem.</h4>
+                        <ol class="no-color">
+                            <li>Fusce id nunc tellus. </li>
+                            <li>Maecenas vitae ornare arcu. </li>
+                            <li>Phasellus gravida congue turpis, eu bibendum lorem molestie eget. </li>
+                            <li>Phasellus tristique sapien non placerat eleifend.</li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="fourth-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h2>NEW PRODUCT PROMOTION ?</h2>
+                <a href="contact.html">(BE THERE RIGHT NOW)</a>
             </div>
         </div>
     </div>
